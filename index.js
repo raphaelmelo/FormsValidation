78438ec74e0b80d33b43edd4ea72bdd5074a4d13
raphaelmelo <contatowbm@gmail.com>
2021-03-27 12:00:53 -0300

    newsletter input validation

--- index.js
+++ index.js
@@ -4,102 +4,90 @@ const regexEmail = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 const regexSenha8Car1Num = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 function inputSubmitLogin() {
-
-    const inputEmail = document.getElementById('inputEmailLogin').value;
+    const inputEmail = document.getElementById("inputEmailLogin").value;
     if (regexEmail.test(inputEmail)) {
-
     } else {
-        alert("Digite um e-mail valido")
+        alert("Digite um e-mail valido");
     }
-    const inputSenha = document.getElementById('inputSenhaLogin').value;
+    const inputSenha = document.getElementById("inputSenhaLogin").value;
     if (regexSenha8Car1Num.test(inputSenha) || inputSenha === "") {
-        alert("Senha invalida")
+        alert("Senha invalida");
     } else {
-
     }
-
-};
-
-
+}
 
 function inputSubmitCadastroPF() {
-
-    const inputNome = document.getElementById('inputNomePF').value;
+    const inputNome = document.getElementById("inputNomePF").value;
     if (inputNome !== "") {
-
     } else {
-        alert("Nome vázio")
+        alert("Nome vázio");
     }
 
-    const inputEmail = document.getElementById('inputEmailPF').value;
+    const inputEmail = document.getElementById("inputEmailPF").value;
     if (regexEmail.test(inputEmail)) {
-
     } else {
-        alert("Email Invalido")
+        alert("Email Invalido");
     }
 
-    const inputCPF = document.getElementById('inputCPF').value;
+    const inputCPF = document.getElementById("inputCPF").value;
     if (regexCPF.test(inputCPF)) {
-
     } else {
-        alert("CPF Invalido")
+        alert("CPF Invalido");
     }
 
-    const inputEndereco = document.getElementById('inputEnderecoPF').value;
+    const inputEndereco = document.getElementById("inputEnderecoPF").value;
     if (inputEndereco !== "") {
-
-
     } else {
-        alert("Endereço vázio")
+        alert("Endereço vázio");
     }
 
-    const inputSenha = document.getElementById('inputSenhaPF').value;
+    const inputSenha = document.getElementById("inputSenhaPF").value;
     if (inputSenha !== "") {
-
     } else {
-        alert("Senha vázia")
+        alert("Senha vázia");
     }
 }
 
 function inputSubmitCadastroPJ() {
-
-    const inputNomePJ = document.getElementById('inputNomePJ').value;
+    const inputNomePJ = document.getElementById("inputNomePJ").value;
     if (inputNomePJ !== "") {
-        console.log("nome recebido")
-
+        console.log("nome recebido");
     } else {
-        alert("nome vazio")
+        alert("nome vazio");
     }
 
-    const inputEmailPJ = document.getElementById('inputEmailPJ').value;
+    const inputEmailPJ = document.getElementById("inputEmailPJ").value;
     if (regexEmail.test(inputEmailPJ)) {
-        console.log("email recebido")
-
+        console.log("email recebido");
     } else {
-        alert("Email Invalido")
+        alert("Email Invalido");
     }
 
-    const inputCNPJ = document.getElementById('inputCNPJ').value;
+    const inputCNPJ = document.getElementById("inputCNPJ").value;
     if (inputCNPJ !== regexCNPJ) {
-        console.log("cnpj recebido")
-
+        console.log("cnpj recebido");
     } else {
-        alert("cnpj Invalido")
+        alert("cnpj Invalido");
     }
-    const inputEnderecoPJ = document.getElementById('inputEnderecoPJ').value;
+    const inputEnderecoPJ = document.getElementById("inputEnderecoPJ").value;
     if (inputEnderecoPJ !== "") {
-        console.log("Endereço recebido")
+        console.log("Endereço recebido");
     } else {
-        alert("endereco Invalido")
+        alert("endereco Invalido");
     }
-    const inputSenhaPJ = document.getElementById('inputSenhaPJ').value;
-    if (inputSenhaPJ === "") {
-        console.log("Senha recebido")
+    const inputSenhaPJ = document.getElementById("inputSenhaPJ").value;
+    if (inputSenhaPJ !== "") {
+        console.log("Senha recebido");
     } else {
-        alert("Senha Invalida")
+        alert("Senha Invalida");
     }
-
 }
 
-const inputNewsLetter = document.getElementById('inputEmailPF').value;
-
+function inputSubmitNewsLetter() {
+    const inputNewsLetter = document.getElementById("submitNewsLetter").value;
+    if (regexEmail.test(inputNewsLetter)) {
+        alert("email valido");
+    } else {
+        alert("email Invalido");
+    }
+}
